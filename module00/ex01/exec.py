4b5ea40ffb351f95dd058bd225013dd1072917d7
Rafaela Ferraz Martin <fmartin.rafaela@gmail.com>
2024-06-10 21:04:33 -0300

    feat: module00 ex01

---
+++ module00/ex01/exec.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def main():
+    if len(sys.argv) < 2:
+        return
+    
+    string = ""
+    for arg in sys.argv[1:]:
+        string += arg + " "
+        
+    string = string.strip()
+    print(string[::-1].swapcase())
+
+
+if __name__ == '__main__':
+    main()
