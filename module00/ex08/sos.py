bdec4099fe7b4e5a66946a2a5378b9b9d27357c2
Rafaela Ferraz Martin <fmartin.rafaela@gmail.com>
2024-06-11 01:54:40 -0300

    feat: module00 ex09

---
+++ module00/ex08/sos.py
@@ -0,0 +1,38 @@
+import sys
+
+
+def morse_dict():
+    return {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '.': '.-.-.-', ',': '--..--',
+    '?': '..--..', "'": '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...',
+    ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-', '"': '.-..-.',
+    '$': '...-..-', '@': '.--.-.', ' ': '/'
+}
+
+
+def validate():
+    if len(sys.argv) < 2:
+        return False
+    for text in sys.argv[1:]:
+        for char in text:
+            if not char.isalnum() and not char.isspace():
+                print("ERROR")
+                return False
+    return True
+
+
+def main():
+    if validate():
+        text = ' '.join(item.strip() for item in sys.argv[1:])
+        morse_text = []
+        for letter in text:
+            morse_text.append(morse_dict()[letter.upper()])
+        print(' '.join(morse_text))
+
+
+if __name__ == '__main__':
+    main()
