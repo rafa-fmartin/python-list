f2e236347713717f353e5cccb0f828685b68b16e
Rafaela Ferraz Martin <fmartin.rafaela@gmail.com>
2024-06-18 00:15:11 -0300

    feat: start module01 ex00

---
+++ module01/ex00/recipe.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+class Recipe():
+    def __init__(
+        self,
+        name: str,
+        cooking_lvl: int,
+        cooking_time: int,
+        ingredients: List,
+        recipe_type: str,
+        description: str = ""
+    ):
+        self.name = name        
+        if cooking_lvl in range (1, 6):
+            self.cooking_lvl = cooking_lvl
+        else:
+            print("cooking_lvl")
+            return
+        if cooking_time >= 0:
+            self.cooking_time = cooking_time
+        else:
+            print("cooking_time")
+            return
+        self.ingredients = ingredients
+        if recipe_type not in ["starter", "lunch", "dessert"]:
+            self.recipe_type = recipe_type
+        else:
+            print("")
+            return
+        self.description = description
+        
+    def __str__(self):
+        txt = """Recipe: {}
+    Cooking level: {}
+    Cooking time: {} minutes
+    Ingredients: {}
+    Description: {}
+    Recipe type: {}""".format(
+        self.name, 
+        self.cooking_lvl, 
+        self.cooking_time,
+        ', '.join(self.ingredients)[:-2],
+        self.description,
+        self.recipe_type
+    )
+        return txt
---
+++ module01/ex00/test.py
@@ -0,0 +1,9 @@
+from book import Book
+from recipe import Recipe
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
